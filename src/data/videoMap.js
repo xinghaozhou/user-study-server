229e91ba01dc87a25869fb3d771e40b287dde2d2
xinghaozhou <xinghao@umich.edu>
2025-08-05 13:34:36 -0400

    randomized order

--- src/data/videoMap.js
+++ src/data/videoMap.js
@@ -1,41 +1,41 @@
 export const VIDEO_URLS = {
-    user01: {
+    user04: {
       long: "https://www.youtube.com/embed/M6wsmI8XAXw",
       short: "https://www.youtube.com/embed/BxjMEVXuprw"
     },
-    user02: {
+    user09: {
       long: "https://www.youtube.com/embed/SlYrOa69i7co",
       short: "https://www.youtube.com/embed/LIJ4y_-OrWk"
     },
-    user03: {
+    user02: {
       long: "https://www.youtube.com/embed/TMB2NCeD9uY",
       short: "https://www.youtube.com/embed/kSYV07E4l50"
     },
-    user04: {
+    user07: {
       long: "https://www.youtube.com/embed/oRq6vN3nU7M",
       short: "https://www.youtube.com/embed/X_6XiRC7RSw"
     },
-    user05: {
+    user01: {
       long: "https://www.youtube.com/embed/PX_QX1QSc9U",
       short: "https://www.youtube.com/embed/cgEqV-bUlvM"
     },
-    user06: {
+    user10: {
       long: "https://www.youtube.com/embed/mLjYT6y9RWk",
       short: "https://www.youtube.com/embed/IXyjNqQKo_k"
     },
-    user07: {
+    user05: {
       long: "https://www.youtube.com/embed/-R1MMwErIBQ",
       short: "https://www.youtube.com/embed/zn-jeLoHFks"
     },
-    user08: {
+    user06: {
       long: "https://www.youtube.com/embed/U1YhaNk_Cn0",
       short: "https://www.youtube.com/embed/1uQfqEEaxYQ"
     },
-    user09: {
+    user03: {
       long: "https://www.youtube.com/embed/l9KvpR41c4s",
       short: "https://www.youtube.com/embed/nZo0oLzy2KU"
     },
-    user10: {
+    user08: {
       long: "https://www.youtube.com/embed/8XvBoTooG1Q",
       short: "https://www.youtube.com/embed/GaKdrFHfBZ4"
     },
